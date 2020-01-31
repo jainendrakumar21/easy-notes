0d0422e7f9aa4a110bf5979758ebb839024e9d0e
Narendrakumarkolli <30337207+narendrajava1@users.noreply.github.com>
2020-01-31 21:04:48 +0530

    Update Note.java

--- src/main/java/com/easynotes/model/Note.java
+++ src/main/java/com/easynotes/model/Note.java
@@ -3,6 +3,7 @@ package com.easynotes.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
